a4ad815e3b614a416d3aeaae84bb1a11b3d428db
João Pedro Toledo Martins <joaopedrotoledomartins@gmail.com>
2024-11-05 15:39:44 -0200

    feature: criação da Pasta Aula 01

---
+++ Aula 01/index.php
@@ -0,0 +1,16 @@
+<?php
+    include('../funcoes.php');
+?>
+<html>
+<head>
+    <title>Aula 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Aula do dia 05/11</h1>
+    <?php include('../div.php');?>
+    <?php
+    
+    ?>
+</body>
+</html>
